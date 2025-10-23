285eb2e671065063b6a2f92d055bc1d0b2769069
Persephone <evan.anderson_@outlook.com>
2025-10-22 17:15:35 -0700

    Matrix stuff, det and inverse

---
+++ src/matrix.rs
@@ -0,0 +1,450 @@
+use std::{
+  fmt::Display,
+  ops::{Add, Mul, Neg, Sub},
+};
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum MatrixError
+{
+  MissmatchDimension
+  {
+    lhs: (usize, usize),
+    rhs: (usize, usize),
+  },
+  NonUniformColumnLength,
+  OutOfBounds(usize),
+  NonSquareMatrix,
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct MatrixReal
+{
+  row_count: usize,
+  col_count: usize,
+  data: Vec<Vec<f64>>,
+}
+
+pub type Result<T> = std::result::Result<T, MatrixError>;
+
+impl MatrixReal
+{
+  pub fn new(data: Vec<Vec<f64>>) -> Result<Self>
+  {
+    // data[row][col]
+    if data.len() == 0
+    {
+      return Ok(Self {
+        row_count: 0,
+        col_count: 0,
+        data,
+      });
+    }
+
+    let col_len = data[0].len();
+    if data.iter().any(|x| x.len() != col_len)
+    {
+      return Err(MatrixError::NonUniformColumnLength);
+    }
+
+    Ok(Self {
+      row_count: data.len(),
+      col_count: col_len,
+      data,
+    })
+  }
+
+  pub fn identity(dim: usize) -> Self
+  {
+    let mut data = Vec::new();
+    let mut index = 0;
+    data.resize_with(dim, || {
+      let mut res = Vec::new();
+      let mut current = 0;
+      res.resize_with(dim, || {
+        let old = current;
+        current += 1;
+        if old == index
+        {
+          return 1.0;
+        }
+        else
+        {
+          return 0.0;
+        }
+      });
+      index += 1;
+      res
+    });
+    Self {
+      row_count: dim,
+      col_count: dim,
+      data,
+    }
+  }
+
+  pub const fn rows(&self) -> usize
+  {
+    self.row_count
+  }
+  pub const fn cols(&self) -> usize
+  {
+    self.col_count
+  }
+  pub fn row(&self, idx: usize) -> Result<Vec<&f64>>
+  {
+    if idx >= self.row_count
+    {
+      return Err(MatrixError::OutOfBounds(idx));
+    }
+    Ok(self.data[idx].iter().collect())
+  }
+  pub fn row_mut(&mut self, idx: usize) -> Result<Vec<&mut f64>>
+  {
+    if idx >= self.row_count
+    {
+      return Err(MatrixError::OutOfBounds(idx));
+    }
+    Ok(self.data[idx].iter_mut().collect())
+  }
+
+  pub fn col(&self, idx: usize) -> Result<Vec<&f64>>
+  {
+    if idx >= self.col_count
+    {
+      return Err(MatrixError::OutOfBounds(idx));
+    }
+    Ok(self.data.iter().map(|x| &x[idx]).collect())
+  }
+  pub fn col_mut(&mut self, idx: usize) -> Result<Vec<&mut f64>>
+  {
+    if idx >= self.col_count
+    {
+      return Err(MatrixError::OutOfBounds(idx));
+    }
+    Ok(self.data.iter_mut().map(|x| &mut x[idx]).collect())
+  }
+  pub fn remove_row(mut self, row: usize) -> Result<Self>
+  {
+    if row >= self.rows()
+    {
+      return Err(MatrixError::OutOfBounds(row));
+    }
+    self.data.remove(row);
+    self.row_count -= 1;
+    Ok(self)
+  }
+  pub fn remove_col(mut self, col: usize) -> Result<Self>
+  {
+    if col >= self.cols()
+    {
+      return Err(MatrixError::OutOfBounds(col));
+    }
+    self.data.iter_mut().for_each(|x| {
+      x.remove(col);
+    });
+    self.col_count -= 1;
+    Ok(self)
+  }
+
+  pub fn det(&self) -> Result<f64>
+  {
+    // non-square matrices dont have a determinant
+    if self.rows() != self.cols()
+    {
+      return Err(MatrixError::NonSquareMatrix);
+    }
+
+    // base case
+    if self.rows() == 2
+    {
+      return Ok(self.data[0][0] * self.data[1][1] - self.data[0][1] * self.data[1][0]);
+    }
+
+    // get the coefficient row
+    let coeff: Vec<f64> = self.row(0)?.iter().cloned().cloned().collect();
+
+    // the matrix without the top row
+    let working = self.clone().remove_row(0)?;
+
+    // start positive
+    let mut sign = 1.0;
+    let mut ret = 0.0;
+    for i in 0..coeff.len()
+    {
+      // the eq that is part of the sum
+      // sign * coefficient of the removed column * determinant of the submatrix
+      ret += sign * coeff[i] * working.clone().remove_col(i)?.det()?;
+
+      // flip the sign for the next one
+      sign = -sign;
+    }
+
+    Ok(ret)
+  }
+  pub fn cofactor_matrix(&self) -> Result<Self>
+  {
+    if self.row_count != self.col_count
+    {
+      return Err(MatrixError::NonSquareMatrix);
+    }
+
+    let mut data = Vec::new();
+    data.resize(self.row_count, Vec::new());
+    for i in 0..self.row_count
+    {
+      for j in 0..self.col_count
+      {
+        let sign = if (i + j) % 2 == 0 { 1.0 } else { -1.0 };
+        data[i].push(sign * self.clone().remove_row(i)?.remove_col(j)?.det()?);
+      }
+    }
+
+    Self::new(data)
+  }
+
+  pub fn transpose(&self) -> Self
+  {
+    let mut i = 0;
+    let mut data = Vec::new();
+    data.resize_with(self.cols(), || {
+      let old = i;
+      i += 1;
+      self.col(old).unwrap().iter().map(|x| **x).collect()
+    });
+
+    Self::new(data).unwrap()
+  }
+
+  pub fn inverse(&self) -> Result<Self>
+  {
+    Ok(1.0 / self.det()? * self.cofactor_matrix()?.transpose())
+  }
+}
+
+impl Add for MatrixReal
+{
+  type Output = Result<Self>;
+
+  fn add(self, rhs: Self) -> Self::Output
+  {
+    if self.col_count != rhs.col_count || self.row_count != rhs.row_count
+    {
+      Err(MatrixError::MissmatchDimension {
+        lhs: (self.row_count, self.col_count),
+        rhs: (rhs.row_count, rhs.col_count),
+      })
+    }
+    else
+    {
+      let mut ret = self;
+      ret
+        .data
+        .iter_mut()
+        .flatten()
+        .zip(rhs.data.iter().flatten())
+        .for_each(|(x, y)| *x += *y);
+      Ok(ret)
+    }
+  }
+}
+
+impl Sub for MatrixReal
+{
+  type Output = Result<Self>;
+
+  fn sub(self, rhs: Self) -> Self::Output
+  {
+    self + (-rhs)
+  }
+}
+
+impl Mul<MatrixReal> for f64
+{
+  type Output = MatrixReal;
+
+  fn mul(self, rhs: MatrixReal) -> Self::Output
+  {
+    let mut ret = rhs;
+    ret.data.iter_mut().flatten().for_each(|x| *x *= self);
+    ret
+  }
+}
+
+impl Mul for MatrixReal
+{
+  type Output = Result<Self>;
+
+  fn mul(self, rhs: Self) -> Self::Output
+  {
+    if self.cols() != rhs.rows()
+    {
+      return Err(MatrixError::MissmatchDimension {
+        lhs: (self.rows(), self.cols()),
+        rhs: (rhs.rows(), rhs.cols()),
+      });
+    }
+
+    let mut data = Vec::new();
+    data.resize(self.rows(), Vec::new());
+    data.iter_mut().for_each(|x| x.resize(rhs.cols(), 0.0));
+
+    for row in 0..self.rows()
+    {
+      for col in 0..rhs.cols()
+      {
+        let value = self
+          .row(row)?
+          .iter()
+          .zip(rhs.col(col)?.iter())
+          .map(|(x, y)| *x * *y)
+          .sum::<f64>();
+        data[row][col] = value;
+      }
+    }
+
+    Self::new(data)
+  }
+}
+
+impl Neg for MatrixReal
+{
+  type Output = Self;
+
+  fn neg(self) -> Self::Output
+  {
+    -1.0 * self
+  }
+}
+
+impl Display for MatrixReal
+{
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+  {
+    write!(f, "[")?;
+    for i in 0..self.data.len()
+    {
+      let v = &self.data[i];
+      write!(f, "[")?;
+      for j in 0..v.len()
+      {
+        write!(f, "{}", v[j])?;
+        if j + 1 != v.len()
+        {
+          write!(f, ", ")?;
+        }
+      }
+      if i + 1 != self.data.len()
+      {
+        write!(f, ", ")?;
+      }
+      write!(f, "]")?;
+    }
+    write!(f, "]")
+  }
+}
+
+#[cfg(test)]
+mod tests
+{
+  use crate::matrix::MatrixReal;
+
+  #[test]
+  fn identity()
+  {
+    assert_eq!(
+      MatrixReal::identity(2),
+      MatrixReal::new(vec![vec![1.0, 0.0], vec![0.0, 1.0]]).unwrap()
+    );
+    assert_eq!(
+      MatrixReal::identity(3),
+      MatrixReal::new(vec![
+        vec![1.0, 0.0, 0.0],
+        vec![0.0, 1.0, 0.0],
+        vec![0.0, 0.0, 1.0]
+      ])
+      .unwrap()
+    );
+  }
+  #[test]
+  fn det()
+  {
+    assert_eq!(
+      MatrixReal::new(vec![
+        vec![1.0, 4.0, 2.0, 3.0],
+        vec![2.0, 3.0, 7.0, 0.0],
+        vec![3.0, 7.0, 4.0, 4.0],
+        vec![9.0, 1.0, 5.0, 0.0]
+      ])
+      .unwrap()
+      .det()
+      .unwrap(),
+      205.0
+    );
+  }
+
+  #[test]
+  fn cofactor_matrix()
+  {
+    assert_eq!(
+      MatrixReal::new(vec![
+        vec![1.0, 4.0, 2.0, 3.0],
+        vec![2.0, 3.0, 7.0, 0.0],
+        vec![3.0, 7.0, 4.0, 4.0],
+        vec![9.0, 1.0, 5.0, 0.0]
+      ])
+      .unwrap()
+      .cofactor_matrix()
+      .unwrap(),
+      MatrixReal::new(vec![
+        vec![-32.0, -212.0, 100.0, 295.0],
+        vec![-21.0, -11.0, 40.0, -5.0],
+        vec![24.0, 159.0, -75.0, -170.0],
+        vec![23.0, -27.0, 5.0, 25.0]
+      ])
+      .unwrap()
+    );
+  }
+
+  #[test]
+  fn transpose()
+  {
+    assert_eq!(
+      MatrixReal::new(vec![
+        vec![1.0, 4.0, 2.0, 3.0],
+        vec![2.0, 3.0, 7.0, 0.0],
+        vec![3.0, 7.0, 4.0, 4.0],
+        vec![9.0, 1.0, 5.0, 0.0]
+      ])
+      .unwrap()
+      .transpose(),
+      MatrixReal::new(vec![
+        vec![1.0, 2.0, 3.0, 9.0],
+        vec![4.0, 3.0, 7.0, 1.0],
+        vec![2.0, 7.0, 4.0, 5.0],
+        vec![3.0, 0.0, 4.0, 0.0]
+      ])
+      .unwrap()
+    );
+  }
+
+  #[test]
+  fn inverse()
+  {
+    assert_eq!(
+      MatrixReal::new(vec![
+        vec![1.0, 2.0, 3.0],
+        vec![3.0, -2.0, 1.0],
+        vec![4.0, 1.0, 1.0],
+      ])
+      .unwrap()
+      .inverse()
+      .unwrap(),
+      MatrixReal::new(vec![
+        vec![-3.0 / 32.0, 1.0 / 32.0, 1.0 / 4.0],
+        vec![1.0 / 32.0, -11.0 / 32.0, 1.0 / 4.0],
+        vec![11.0 / 32.0, 7.0 / 32.0, -1.0 / 4.0]
+      ])
+      .unwrap()
+    );
+  }
+}
